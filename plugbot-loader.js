8c314d9a2685721d298e46e5ef7a793087c93348
Mafufa <metukas@gmail.com>
2013-10-20 14:26:29 +0300

    Create plugbot-loader.js

---
+++ plugbot-loader.js
@@ -0,0 +1,14 @@
+var path = 'https://raw.github.com/connergdavis/Plugbot/master';
+
+function print(msg)
+
+  $('#chat-messages').append('<div class="chat-update"><span class="chat-text" style="color:#ffd900;font-weight:bold">' + msg + '</span></div>');
+
+var scriptFail = window.setTimeout(function() {
+  print('plug.bot couldn\'t load.  Check out the "Basic Troubleshooting" section of the readme on github.com.  Chances are it\'s a simple fix.');
+}, 2000);
+
+$.getScript(path + '/plugbot.js', function() {
+  print('plug.bot says hello.');
+  window.clearTimeout(scriptFail);
+});
