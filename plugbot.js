4d98144abfe7cff5823c230bfa03e993ade05f65
Mafufa <metukas@gmail.com>
2013-10-20 14:40:06 +0300

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -1,3 +1,43 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * TERMS OF REPRODUCTION USE
+ *
+ * 1. Provide a link back to the original repository (this repository), as
+ *    in, https://github.com/ConnerGDavis/Plugbot, that is well-visible
+ *    wherever the source is being reproduced.  For example, should you
+ *    display it on a website, you should provide a link above/below that
+ *    which the users use, titled something such as "ORIGINAL AUTHOR".
+ *
+ * 2. Retain the GNU GPL, TERMS OF REPRODUCTION USE and author header comments at
+ *    the beginning of any redistributed files you produce.
+ *
+ * Failure to follow these terms will result in me getting very angry at you
+ * and having your software tweaked or removed if possible.  Either way, you're
+ * still an idiot for not following such a basic rule, so at least I'll have
+ * that going for me.
+ */
+
+/*
+ * @author  Conner Davis (Fugitive. on Plug.dj)
+ */
+ 
+/*
+ * Whether the user has currently enabled auto-woot.
+ */
 var autowoot;
 /*
  * Whether the user has currently enabled auto-queueing.
@@ -635,3 +675,4 @@ function onCookiesLoaded()
   displayUI();
   initUIListeners();
 }
+
